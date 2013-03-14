b5710a074dc07e31d08818008d7cff9aa96879c4
roliv <roliv@stonemor.com>
2013-03-14 16:12:10 -0400

    Added custom json exception strategy

--- config/application.config.php
+++ config/application.config.php
@@ -9,8 +9,7 @@
  */
 return array(
     'modules' => array(
-        'Application',
-        'Dips'
+        'Roles',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
