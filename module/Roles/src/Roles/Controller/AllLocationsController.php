96f99910ec43356fdb89debc0c532e218bc79935
Unknown <roliver3@gmail.com>
2018-02-21 20:02:01 -0500

    New puphpet config and endpoint updates
    Created new config for upgraded vmware-fusion
    Updated JSON output to return nested JSON payload

--- module/Roles/src/Roles/Controller/AllLocationsController.php
+++ module/Roles/src/Roles/Controller/AllLocationsController.php
@@ -31,7 +31,21 @@ class AllLocationsController extends ApiBaseController
             $this->getServiceLocator()->get('Zend\Log')->crit($logData);
             throw new \Exception($e->getMessage());
         }
-
         return $this->getJson($this->data);
+    }
+
+
+    public function createCustomJSON($data)
+    {
+        foreach ($data as $items) {
+            foreach ($items as $key => $value) {
+                if ($key == 'unit_name') {
+                    list($locNbr, $locName) = preg_split('/-/', $value);
+                }
+                $jsonArray['CorpstructData'][] = [
+                    $key => $value
+                ];
+            }
+        }
     }
 }
