a2eeec113dab2bb90344a42a5cfb9b7ecc8221a0
Julian <jptak@stonemor.com>
2018-09-07 10:04:08 -0400

    KEYSERVERAPI-8 Removed troublesome code in init.sql.

--- main/configs/sql/init.sql
+++ main/configs/sql/init.sql
@@ -4,6 +4,6 @@ CREATE DATABASE `apikey`;
 CREATE USER '--DB_USER_NAME--'@'localhost' IDENTIFIED BY '--DB_USER_PASSWORD--';
 GRANT ALL PRIVILEGES ON *.* TO '--DB_USER_NAME--'@'localhost' WITH GRANT OPTION;
 
-USE `rollup`;
+// USE `rollup`;
 
-UPDATE `user_master` SET `user_profile` = '--DS USER--', `email` = '--DS EMAIL--', `carlib_shortname` = '--DS USER--' WHERE `id` = '20648859';
+// UPDATE `user_master` SET `user_profile` = '--DS USER--', `email` = '--DS EMAIL--', `carlib_shortname` = '--DS USER--' WHERE `id` = '20648859';
